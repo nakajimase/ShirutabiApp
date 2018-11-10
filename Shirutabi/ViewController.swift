6c0e12b88a5002f69c2a82b1368fc0c961c66c69
nakajimase <seiya.nakajima.jpn@gmail.com>
2018-11-11 00:20:16 +0900

    Initial Commit

---
+++ Shirutabi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shirutabi
+//
+//  Created by 中嶋聖也 on 2018/11/11.
+//  Copyright © 2018 Seiya Nakajima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
